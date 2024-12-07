f77948742bd9d23f101eab2836b6c57f15044b06
ZeeTW <zainab.alafoo@hotmail.com>
2024-12-07 16:10:20 +0300

    added post controller and imported it to server.js

---
+++ controllers/posts.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+//Import Model
+const Post = require('../models/post')
+
+//Routes/ API's / Core Functionality
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -9,5 +9,10 @@ const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const morgan = require('morgan')
 
+//controllers
+const postsCtrl = require ('./controllers/post')
+
+app.use('/posts', postsCtrl)
+
 //port config
 const PORT = process.env.PORT ? process.env.PORT : '3000'
