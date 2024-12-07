09a49597dfd196d2f84274cd5dd0523d044ae3ae
ZeeTW <zainab.alafoo@hotmail.com>
2024-12-07 23:51:20 +0300

    added database connection and index for posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,5 +7,11 @@ const Post = require('../models/post')
 //Routes/ API's / Core Functionality
 
 
+//landing page
+router.get('/', async(req,res)=>{
+  const posts = await Post.find()
+  res.render('posts/index.ejs', {posts})
+})
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const dotenv = require('dotenv')
 dotenv.config()
 const express = require('express')
-const session = require('express-session')
-const passUsertoView = require('./middleware/pass-user-to-view')
+// const session = require('express-session')
+// const passUsertoView = require('./middleware/pass-user-to-view')
 const app = express()
 
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const morgan = require('morgan')
 
+// data connection
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected', () => {
+  console.log(`connected to MongoDB Database: ${mongoose.connection.name}`)
+})
+
 //controllers
-const postsCtrl = require ('./controllers/post')
+const postsCtrl = require ('./controllers/posts')
 
 app.use('/posts', postsCtrl)
 
 //port config
 const PORT = process.env.PORT ? process.env.PORT : '3000'
+
+//listen for the HTTP requests
+app.listen(PORT, () => {
+  console.log(`Travel Buddies App is listening for requests on port ${PORT}`)
+})
