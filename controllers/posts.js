f9b559762c26feb9fe5a651b5b016df84cbf2cb7
ZeeTW <zainab.alafoo@hotmail.com>
2024-12-08 09:37:49 +0300

    added the new post ejs

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,11 +7,14 @@ const Post = require('../models/post')
 //Routes/ API's / Core Functionality
 
 
-//landing page
+//landing post page
 router.get('/', async(req,res)=>{
   const posts = await Post.find()
   res.render('posts/index.ejs', {posts})
 })
 
+router.get('/new', async(req,res)=>{
+  res.render('posts/new.ejs')
+})
 
 module.exports = router
