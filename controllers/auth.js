fe11f4658424d706d9ac24ddf00a6a61cccd3305
unknown <mmxmox@hotmail.com>
2024-12-09 15:39:37 +0300

    profile done almost

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
-
+const isSignedIn = require('../middleware/is-signed-in')
 //routes/API/Controller functions
 
 router.get('/sign-up', (req, res) => {
@@ -52,6 +52,63 @@ router.post('/sign-in', async (req, res) => {
   res.redirect('/')
 })
 
+router.get('/:profile', isSignedIn, async (req, res) => {
+  try {
+    const user = await User.findById(req.session.user._id)
+    res.render('auth/profile', { user})
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('Error No user data!')
+  }
+})
+
+router.get('/:edit.profile', isSignedIn, async (req, res) => {
+  try{
+    const user = await User.findByIdAndUpdate(req.session.user._id)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('Error No user data! ')
+  }
+})
+
+router.post('/:edit-profile', isSignedIn, async (req, res) => {
+  try {
+    const { username, password, age, nationality, gender, married } = req.body
+    const user = await User.findById(req.session.user._id)
+
+    
+    user.username = username || user.username
+
+    user.password = password ? bcrypt.hashSync(password, 10) : user.password
+
+    user.age = age || user.age
+
+    user.nationality = nationality || user.nationality
+
+    user.gender = gender || user.gender
+
+    user.married = married || user.married
+
+    await user.save()
+    res.redirect('/auth/profile')
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('Error updating profile')
+  }
+})
+
+router.post('/:delete-profile', isSignedIn, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.session.user._id)
+    req.session.destroy((err) => {
+      if (err) return res.status(500).send('Error loggong out')
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('Error Deleting profile')
+  }
+})
 
 
 module.exports = router
