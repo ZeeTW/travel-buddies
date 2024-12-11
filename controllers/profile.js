79079a8a54dc8561fe0ef23ff8a50f5839540c55
ZeeTW <zainab.alafoo@hotmail.com>
2024-12-11 11:20:46 +0300

    almost done

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
-const isSignedIn = require('../middleware/is-signed-in')const upload = require('../server')
+const isSignedIn = require('../middleware/is-signed-in')
+const upload = require('../server')
 
 router.get('/profile/show', async (req, res) => {
   const user = await User.findById(req.session.user._id)
@@ -21,7 +22,6 @@ router.get('/profile', isSignedIn, async (req, res) => {
     res.render('profile/show', { user }) // Render profile/show.ejs
   } catch (error) {
     console.log(error)
-    console.log(error);
   }
 })
 // Edit Profile
@@ -34,36 +34,30 @@ router.get('/profile/edit', isSignedIn, async (req, res) => {
   }
 })
 // Update Profile
-router.post('/profile/:id/edit', isSignedIn, upload.single('image'), async (req, res) => {
-  try {
-    const { username, age, nationality, gender } = req.body;
-    req.body.profileImage=req.file.filename
-    console.log("req.body" , req.body);
-    const user = await User.findByIdAndUpdate(req.session.user._id, req.body);
-    user.username = username || user.username;
-    user.age = age || user.age;
-    user.nationality = nationality || user.nationality;
-    user.gender = gender || user.gender;
+router.post(
+  '/profile/:id/edit',
+  isSignedIn,
+  upload.single('image'),
+  async (req, res) => {
+    try {
+      const { username, age, nationality, gender } = req.body
+      req.body.profileImage = req.file.filename
+      console.log('req.body', req.body)
+      const user = await User.findByIdAndUpdate(req.session.user._id, req.body)
+      user.username = username || user.username
+      user.age = age || user.age
+      user.nationality = nationality || user.nationality
+      user.gender = gender || user.gender
 
-
-    const { username, age, nationality, gender } = req.body
-
-    const user = await User.findByIdAndUpdate(req.session.user._id, req.body)
-
-    user.username = username || user.username
-    user.age = age || user.age
-    user.nationality = nationality || user.nationality
-    user.gender = gender || user.gender
-
-    await user.save()
-    res.redirect('/profile/show')
-  } catch (error) {
-    console.log(error)
-    console.log(error);
+      await user.save()
+      res.redirect('/profile/show')
+    } catch (error) {
+      console.log(error)
+      console.log(error)
+    }
   }
-})
+)
 
-});
 // Delete Profile
 router.post('/profile/delete', isSignedIn, async (req, res) => {
   try {
@@ -78,6 +72,3 @@ router.post('/profile/delete', isSignedIn, async (req, res) => {
 })
 
 module.exports = router
-
-});
-module.exports = router;
